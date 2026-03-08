24caa588fbd2dd329f2e6f31859bf4bdd4ed60c5
Jonas Martin Smidt <jos005@edu.zealand.dk>
2026-03-08 16:21:31 +0100

    Null exceptions test

--- TestsLeg/DatabaseMotion/ServicesTests/HotelServiceTests.cs
+++ TestsLeg/DatabaseMotion/ServicesTests/HotelServiceTests.cs
@@ -16,64 +16,74 @@ namespace TestsLeg.DatabaseMotion.Services
     public class HotelServiceTests
     {
         //TODO: test fixture 
+        private readonly Mock<IHotelRepository> _mockRepository;
+        private readonly HotelService _hotelService;
+        private readonly InsertHotelService _insertHotelService;
 
-
-
-        [Fact]
-        //naming is like this CLassName_MethodName_WhatWeWantToHappen
-        public void HotelService_GetHotels_ReturnListofHotels() {
-            //Arrange
+        // Konstruktøren kører før hver test 
+        public HotelServiceTests() {
             // -------------lav en fake database
             // Vi laver af type interface fordi:
             //Moq genrerer en ny klasse som der implemeterer interfacet, det kan den godt finde ud af.
             // Hvis vi brugte HotelRepository direkte ville moq kører den rigtige kode som rammer databasen
             // hvilket vi slet ikke ønsker
-            Mock<IHotelRepository> mockRepository = new Mock<IHotelRepository>();
+            _mockRepository = new Mock<IHotelRepository>();
+            //----------hent den faktiske fake instans og giv services den fake database--------------
+            // Vi opretter en rigtig HotelService, men giver den vores FAKE repository
+            // .Object = den faktiske fake instans som Moq har genereret der implementerer IHotelRepository
+            // VI narrer Hotelservice til at tro den får den rigtige IHotelService selvom den er fake
+            _hotelService = new HotelService(_mockRepository.Object);
+            _insertHotelService = new InsertHotelService(_mockRepository.Object);
+        }
 
+        [Fact]
+        public void HotelsService_GetHotels_ReturnEmptyList() {
+            //Arrange
+            List<Hotel> emptyHotelList = new List<Hotel>();
+
+            _mockRepository.Setup(repo => repo.GetAll()).Returns(emptyHotelList);
+            //Act
+            IEnumerable<Hotel> result = _hotelService.GetHotels();
+            //Assert
+            result.Should().NotBeNull(); // altid null exceptions først fordi empty skal ikke kalde null exceptions
+            result.Should().BeEmpty(); 
+       
+        }
+
+        [Fact]
+        //naming is like this CLassName_MethodName_WhatWeWantToHappen
+        public void HotelService_GetHotels_ReturnListofHotels() {
+            //Arrange
+        
             List<Hotel> forventetListe = new List<Hotel>
             {
                 new Hotel {Name = "Grand Hotel"},
                 new Hotel {Name = "Seaside inn"}
 
             };
-
             // --------fortæl den hvad den skal returnere-------------
             // Vi fortæller mock'en: "Når GetAll() bliver kaldt, returner vores forventetListe"
             // Uden Setup ville mock'en returnere null, fordi den er en tom fake der ikke ved hvad den skal gøre
-            mockRepository.Setup(repo => repo.GetAll()).Returns(forventetListe);
-
-            //----------hent den faktiske fake instans og giv services den fake database--------------
-            // Vi opretter en rigtig HotelService, men giver den vores FAKE repository
-            // .Object = den faktiske fake instans som Moq har genereret der implementerer IHotelRepository
-            // VI narrer Hotelservice til at tro den får den rigtige IHotelService selvom den er fake
-            HotelService hotelService = new HotelService(mockRepository.Object); 
+            _mockRepository.Setup(repo => repo.GetAll()).Returns(forventetListe);
 
             //Act
-            IEnumerable<Hotel> result = hotelService.GetHotels();
+            IEnumerable<Hotel> result = _hotelService.GetHotels();
             //Assert
             Assert.NotNull(result);
             Assert.Equal(2,result.Count());
-
-
         }
 
         [Fact]
         public void HotelService_InsertHotel_ReturnHotel() {
             //Arrange
-            // make a new fake class that implements the same interface. husk at tænk over hvad skal vi bruge for at lave vores test?
-            Mock<IHotelRepository> moqRepository = new Mock<IHotelRepository>();
 
             // lav et nyt hotel
             Hotel newHotel = new Hotel() { Name = "Fake Hostel" };
 
             // den er dum og skal fortælle hvad den skal retunerer når den bliver kaldt
-            moqRepository.Setup(repo => repo.NewHotel(newHotel)).Returns(newHotel);
-
-            // nu skal vi opretholde instantiere en rigtig InsertHotelService klasse men den skal pege på vores fake moq object
-
-            InsertHotelService insertHotelService = new InsertHotelService(moqRepository.Object);
+            _mockRepository.Setup(repo => repo.NewHotel(newHotel)).Returns(newHotel);
             //Act
-            Hotel result = insertHotelService.NewHotel(newHotel);
+            Hotel result = _insertHotelService.NewHotel(newHotel);
 
             //Assert
             
