bea5f1de4074166b38a67dac4a5bd8de0839b2fd
Jonas Martin Smidt <jonassmidt@hotmail.dk>
2026-03-31 10:22:22 +0200

    documentation and testing automation

--- DatabaseMotion/Services/EFServices/InsertHotelService.cs
+++ DatabaseMotion/Services/EFServices/InsertHotelService.cs
@@ -11,6 +11,11 @@ namespace DatabaseMotion.Services.EFServices
         public InsertHotelService(IHotelRepository repository) {
             _repository = repository;
         }
+        /// <summary>
+        /// Creates a new hotel entry in the repository.
+        /// </summary>
+        /// <param name="hotel">The hotel entity to be added.</param>
+        /// <returns>The newly created hotel entity.</returns>
         public Hotel NewHotel(Hotel hotel) {
             return _repository.NewHotel(hotel);
             
